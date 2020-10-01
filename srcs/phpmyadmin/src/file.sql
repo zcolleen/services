9078da03bdbd061e4f3f13374226fb2fa43173e4
Semen Jejgin <zcolleen@mi-r3.msk.21-school.ru>
2020-10-01 18:08:28 +0300

    phpmyaddmin

--- srcs/phpmyadmin/src/file.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
-GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;
-UPDATE mysql.user SET plugin = 'mysql_native_password' WHERE User = 'root';
-FLUSH PRIVILEGES;
