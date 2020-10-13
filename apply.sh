9650c9641ab5b268d0ddae64282aab4a9023d5f0
Semen Jejgin <zcolleen@ox-r5.msk.21-school.ru>
2020-10-14 00:00:59 +0300

    ftps

--- apply.sh
+++ apply.sh
@@ -2,3 +2,4 @@ kubectl apply -f persistant_volumes.yaml
 kubectl apply -f srcs/nginx/mynginx.yaml
 kubectl apply -f srcs/phpmyadmin/phpmyadmin.yaml
 kubectl apply -f srcs/mysql/mysql.yaml
+kubectl apply -f srcs/ftps/ftps.yaml
