8eab3e2ffac878ce7f43abc698b96cc80285e28a
Semen Jejgin <zcolleen@ox-r5.msk.21-school.ru>
2020-10-15 22:43:47 +0300

    wp

--- minikube_start.sh
+++ minikube_start.sh
@@ -1,4 +1,4 @@
-minikube start
+minikube start  --disk-size='5000mb'
 minikube addons enable metallb
 minikube addons enable dashboard
 minikube dashboard
--- srcs/mysql/src/start.sh
+++ srcs/mysql/src/start.sh
@@ -2,5 +2,6 @@ openrc default
 /etc/init.d/mariadb setup
 rc-service mariadb start
 mysql < file.sql
+mysql < wordpress.sql
 rc-service mariadb stop
 /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
