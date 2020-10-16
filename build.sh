3ebb98555af3c2e4303ac065e0c85b99bbee8027
Semen Jejgin <zcolleen@ox-r5.msk.21-school.ru>
2020-10-16 21:13:05 +0300

    influxdb

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
-docker build -t mynginx srcs/nginx/
-docker build -t phpmyadmin srcs/phpmyadmin/
-docker build -t mysql srcs/mysql/
-docker build -t ftps srcs/ftps/
-docker build -t wordpress srcs/wordpress/
+#docker build -t mynginx srcs/nginx/
+#docker build -t phpmyadmin srcs/phpmyadmin/
+#docker build -t mysql srcs/mysql/
+#docker build -t ftps srcs/ftps/
+#docker build -t wordpress srcs/wordpress/
+docker build -t grafana srcs/grafana/
